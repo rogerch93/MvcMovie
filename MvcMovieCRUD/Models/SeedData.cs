d0145568f4554abb2008e557118fdce50b26c464
rogerch93 <rogerinhochaves@hotmail.com>
2021-09-26 10:03:04 -0300

    BCO em app MVC asp.net core

---
+++ MvcMovieCRUD/Models/SeedData.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System.Linq;
+
+
+namespace MvcMovie.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using(var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                DbContextOptions<MvcMovieContext>>()))
+            {
+                //Look for any movies;
+                if (context.Movie.Any())
+                {
+                    return; //DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Quando Harry Conheceu Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romance Comédia",
+                        Price = 7.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Caça Fantasmas",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comédia",
+                        Price = 8.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Caça Fantasmas 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comédia",
+                        Price = 7.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Ocidental",
+                        Price = 3.99M
+    
+                    }
+                 );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- MvcMovieCRUD/Program.cs
+++ MvcMovieCRUD/Program.cs
@@ -1,11 +1,11 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MvcMovie.Data;
+using MvcMovie.Models;
+
 
 namespace MvcMovie
 {
@@ -13,7 +13,23 @@ namespace MvcMovie
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch(Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Ocorreu um erro semeando DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
