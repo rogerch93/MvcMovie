e0be171d9616a33988df0a593cb831e172c23b8c
rogerch93 <rogerinhochaves@hotmail.com>
2021-10-04 06:19:49 -0300

    adc camp pesquisa

--- MvcMovieCRUD/Models/Movie.cs
+++ MvcMovieCRUD/Models/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,11 +10,19 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [Display(Name ="Titulo")]
         public string Title { get; set; }
 
+        [Display(Name ="Data de Lançamento")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [Display(Name ="Gênero")]
         public string Genre { get; set; }
+
+        [Display(Name ="Preço")]
+        [Column(TypeName ="decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
